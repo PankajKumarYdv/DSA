751dd2909ced82fa4bdb093972c124a63c15f7e2
PankajKumarYdv <pankajkr2699@gmail.com>
2026-01-12 22:40:30 +0530

    test

--- 02_DataType/unicode.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class unicode {
-    public static void main(String arg[]){
-        for(char c = 0x0900; c <= 0x0970; c++)
-            System.out.println(c+" ");
-    }
-}
