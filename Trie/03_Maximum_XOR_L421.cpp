29484490a0aa7d913d28bba94f54799670751052
PankajKumarYdv <pankajkr2699@gmail.com>
2026-03-26 18:02:29 +0530

    leetcode 421

--- Trie/03_Maximum_XOR_L421.cpp
+++ Trie/03_Maximum_XOR_L421.cpp
@@ -1,4 +1,4 @@
-// Maximum Xor of two number in an array
+// Maximum Xor of two number in an array Leetcode : 421 
 
 #include <iostream> 
 #include <vector> 
@@ -11,14 +11,15 @@ class Leetcode{
 
 
     // XOR ON Array
-    vector<int> xorOperation(vector<int>& nums){
+    int xorOperation(vector<int>& nums){
         vector<int> result; 
         for(int i = 0; i < nums.size(); i++){
             for(int j = i+1; j < nums.size(); j++){
                 result.push_back(nums[i] xor nums[j]); 
             }
         }
-        return result; 
+        int ans = *max_element(result.begin(),result.end());
+        return  ans; 
     }
 }; 
 
@@ -26,9 +27,7 @@ int main(){
     Leetcode m; 
 
     vector<int> nums = {3,10,5,25,2,8}; 
-    vector <int> xorResult = m.xorOperation(nums); 
-    for(auto num : xorResult){
-        cout << num << " " << endl; 
-    }
+    int xorResult = m.xorOperation(nums); 
+    cout << xorResult << endl; 
 
 }
