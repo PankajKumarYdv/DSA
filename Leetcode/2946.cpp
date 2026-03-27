acf530d42c4a2b43767f2e32a5ebbd1aafc90747
PankajKumarYdv <pankajkr2699@gmail.com>
2026-03-27 21:27:09 +0530

    matrix

---
+++ Leetcode/2946.cpp
@@ -0,0 +1,19 @@
+/*
+    2946. Matrix Similarity After Cyclic Shifts
+*/
+
+#include <iostream> 
+#include <vector> 
+using namespace std; 
+
+class Leetcode{
+    public: 
+        bool areSimilar(vector<vector<int>>& mat, int k){
+
+        }
+}; 
+
+int main(){
+    Leetcode k; 
+    
+}
---
+++ cpp/vector/matrix.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <vector> 
+using namespace std; 
+
+int main(){
+    vector<vector<int>> matrix(5,vector <int>(4)); 
+
+    int m = matrix.size(); 
+    int n = matrix[0].size(); 
+    int mxn = m*n; 
+    
+    cout << "m : " << m << " n : " << n << " Total Element : " << mxn << endl; 
+
+    for(it )
+
+    // m - 5 and n - 4; 
+
+
+     
+}
